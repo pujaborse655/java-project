62e3f32d3bfe5500b8db3ebfdfe63a97a64a69a7
pujaborse655 <83423136+pujaborse655@users.noreply.github.com>
2022-12-19 22:17:30 +0530

    Update HelloWorldController.java

--- src/main/java/com/stacksimplify/ccdemo/HelloWorldController.java
+++ src/main/java/com/stacksimplify/ccdemo/HelloWorldController.java
@@ -8,10 +8,7 @@ public class HelloWorldController {
 
 	@GetMapping("/hello")
 	public String sayHello() {
-		return "Welcome to Java Application
-			Java is the Queen of programming languages. It beholds the pride of the most preferred programming language by software developers!
-	
-   			Come and Explore Java!";
+		return "Welcome to Java Application";
 	}
 	
 }
